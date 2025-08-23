e9863aa4f53ae8cedbb535724be61c3103e67a60
VitaliiRublevskii <vitrub76@gmail.com>
2025-08-23 12:11:09 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,20 +1,19 @@
 import axios from 'axios';
 
-const token = localStorage.getItem("token");
 
 export const axiosInstance = axios.create({
   baseURL: "https://bookit.pp.ua/",
-  //baseURL: "https://bookit.pp.ua",
   withCredentials: true,
-  headers: {
-    Authorization: token ? `Bearer ${token}` : "",
-  },
 });
 
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
+ } else {
+   if (config.headers && 'Authorization' in config.headers) {
+     delete config.headers.Authorization;
+   }
   }
   return config;
 });
